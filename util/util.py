11d10b71c6765b6f5d09d3e5b9180aa19c856bba
Zachary Yu <1564026260@qq.com>
2019-11-01 13:41:12 +0800

    V1.0 + onehot

--- util/util.py
+++ util/util.py
@@ -130,3 +130,14 @@ def model_visual(your_model,input_shape):
     :return:
     '''
     print(summary(your_model, input_size=input_shape))
+
+def one_hot(label):
+    """
+    将一维列表转换为独热编码
+    """
+    label = label.resize_(batch_size, 1)
+    m_zeros = torch.zeros(batch_size, class_num)
+    # 从 value 中取值，然后根据 dim 和 index 给相应位置赋值
+    onehot = m_zeros.scatter_(1, label, 1)  # (dim,index,value)
+
+    return onehot.numpy()  # Tensor -> Numpy
